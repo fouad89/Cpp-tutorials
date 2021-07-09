536eb37ab3c13a78d36e53730b051b67c68dc661
Fouad <fouad89ahmed@gmail.com>
2021-07-09 13:26:58 +0100

    first week

---
+++ Week 1/operations.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+/*
+Fundamental types: 
+
+bool // Boolean, possible values are true and false
+char // character, for example, 'a', ' z', and '9'
+int // integer, for example, 1, 42, and 1066
+double // double-precision floating-point number, for example, 3.14 and 299793.0
+
+When type can be deduced from the initial assignment, use:
+auto
+
+Arithmatic operators
+x+y //plus
++x // unar y plus
+x−y //minus
+−x // unar y minus
+x∗y // multiply
+x/y //divide
+x%y // remainder (modulus) for integers
+
+Comparison Operators
+x==y //equal
+x!=y // not equal
+x<y // less than
+x>y // greater than
+x<=y // less than or equal x>=y // greater than or equal
+
+increments and modifying vars:
+
+x+=y //x=x+y
+++x // increment: x = x+1
+ x−=y //x=x-y
+−−x // decrement: x = x-1 
+x∗=y // scaling: x = x*y 
+x/=y // scaling: x = x/y 
+x%=y //x=x%y
+
+
+*/
---
+++ Week 1/square.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+double square(double x){
+    return x*x;
+}
+
+void print_square(double x){
+    cout << "The square of " << x << " is: " << square(x) << "\n";
+}
+
+int main(){
+    print_square(3);
+    return 0;
+}
