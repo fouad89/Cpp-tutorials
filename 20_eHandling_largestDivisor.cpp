4faeae675725deeaeecc735a82a07f33730c6f5d
Fouad <fouad89ahmed@gmail.com>
2021-08-05 12:43:06 +0100

    largest divisor exception handling

--- 20_eHandling_largestDivisor.cpp
+++ 20_eHandling_largestDivisor.cpp
@@ -13,29 +13,49 @@ It takes integer n as an argument and has to work as follows:
         it should print in a single line result=d where d is the returned value.
 3. Otherwise, if the call raises a std::invalid_argument exception, 
     it has to print in a single line the string representation of the raised exception, i.e. its message.
-3.Finally, no matter if the exception is raised or not, it should print in a single line 
+4.Finally, no matter if the exception is raised or not, it should print in a single line 
     returning control flow to caller after any other previously printed output.
-To keep the code quality high, you are advised to have exactly one line printing returning control flow to caller in the body of process_input function.
+To keep the code quality high, you are advised to have exactly one line printing returning 
+control flow to caller in the body of process_input function.
 
  */
 
 int largest_proper_divisor(int n){
-    int max = 0;
-    for (int i = 1; i < n; i++){
-        if (n % i == 0)
+    if (n == 0){
+            throw invalid_argument("Not defined for n=0");
+        } else if (n == 1)
         {
-            if (i > max){
-                max = i;
-            }
+            throw invalid_argument("Not defined for n=1");
+        } 
+    for (int i = n/2; i >= 1; i--){
+        if (n % i == 2){
+            return i;
         }
-        
     }
-    return max;
+    return -1;
 }
 
+
+
+void process_input(int n){
+    try
+    {
+        int d = largest_proper_divisor(n);
+        cout << "result=" << d <<endl;
+        
+    }
+    catch(const invalid_argument& e)
+    {
+        cout << e.what() << endl;
+    }
+    cout << "returning control flow to caller" << endl;
+    
+}
+
+
 int main(){
-    int userInput;
-    cin >> userInput;
-    cout << "Biggest divisor: " << largest_proper_divisor(userInput) << endl;    
+    int n;
+    cin >> n;
+    process_input(n);    
     return 0;
 }
